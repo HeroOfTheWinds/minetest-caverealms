8deaef628a683ba38fc7f44b00e2073630fc6bc1
Craig Robbins <crobbins@localhost.localdomain>
2014-06-20 16:54:33 +1000

    Fixed description for caverealms:stone_with_moss

--- init.lua
+++ init.lua
@@ -119,11 +119,11 @@ minetest.register_node("caverealms:glow_gem", {
 })
 --cave mossy cobble - bluish?
 minetest.register_node("caverealms:stone_with_moss", {
-	description = "Cave Dirt with Grass",
+	description = "Cave Stone with Moss",
 	tiles = {"default_cobble.png^caverealms_moss.png", "default_cobble.png", "default_cobble.png^caverealms_moss_side.png"},
 	is_ground_content = true,
 	groups = {crumbly=3},
-	drop = 'default:dirt',
+	drop = 'default:cobblestone',
 	sounds = default.node_sound_dirt_defaults({
 		footstep = {name="default_grass_footstep", gain=0.25},
 	}),
