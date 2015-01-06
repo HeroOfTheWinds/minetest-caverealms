112c18a01179e267cec9e762424d807fad5a0968
Chris N <swordsman9613@gmail.com>
2015-01-06 13:03:37 -1000

    Counteracting stupidity
    Logic Fail :P

--- nodes.lua
+++ nodes.lua
@@ -536,13 +536,13 @@ minetest.register_node("caverealms:constant_flame", {
 		if pos.y > DM_TOP then
 			minetest.remove_node(pos)
 		end
-		if (minetest.get_modpath("moontest")) then
+		if not (minetest.get_modpath("moontest")) then
 			fire.on_flame_add_at(pos)
 		end
 	end,
 	
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
-		if (minetest.get_modpath("moontest")) then
+		if not (minetest.get_modpath("moontest")) then
 			fire.on_flame_remove_at(pos)
 		end
 	end,
